74bedcdcfed8988c50f7682a1b1f5bc34d7bee84
Kajo <ivan.kajinic@bellabeat.com>
2017-05-09 16:19:10 +0200

    Added spm agent

---
+++ lib/java_buildpack/framework/spm_agent.rb
@@ -0,0 +1,44 @@
+# Cloud Foundry Java Buildpack
+# Copyright 2013-2017 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'fileutils'
+require 'java_buildpack/component/versioned_dependency_component'
+require 'java_buildpack/framework'
+
+module JavaBuildpack
+  module Framework
+
+    # Encapsulates the functionality for enabling zero-touch AppDynamics support.
+    class SpmAgent < JavaBuildpack::Component::VersionedDependencyComponent
+
+      # (see JavaBuildpack::Component::BaseComponent#compile)
+      def compile
+        p 'Adding SPM agent'
+        download_tar(false, @droplet.sandbox, 'Spm Agent')
+        @droplet.copy_resources
+      end
+
+      def release; end
+
+      protected
+
+      # (see JavaBuildpack::Component::VersionedDependencyComponent#supports?)
+      def supports?
+        true
+      end
+    end
+
+  end
+end
